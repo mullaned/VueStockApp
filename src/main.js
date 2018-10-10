c043ad971d380003d09b645df57191b150ae7323
David M <mullaned@gmail.com>
2018-10-10 14:02:48 +0100

    nav setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
+
+Vue.use(VueRouter);
+
+import {routes} from './routes';
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
