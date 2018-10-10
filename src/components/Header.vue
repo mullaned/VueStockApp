9ddeb6277c875b1ed3df66a53d7ac9eb4dedbab6
David M <mullaned@gmail.com>
2018-10-10 18:01:01 +0100

    loading data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -59,9 +59,10 @@ export default {
         }
     },
     methods: {
-        ...mapActions([
-            'randomizeStocks'
-        ]),
+        ...mapActions({
+            randomizeStocks: 'randomizeStocks',
+            fetchData: 'loadData'
+        }),
         endDay(){
             this.randomizeStocks();
         },
@@ -74,7 +75,7 @@ export default {
             this.$http.put('data.json', data);
         },
         loadData() {
-            
+            this.fetchData();
         }
     }
 }
